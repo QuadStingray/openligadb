201fd01b402240d265cd3de6d42c594f344aaeaf
QuadStingray <github@quadstingray.com>
2018-02-20 10:14:41 +0100

    fix in version and release

--- .travis.release.sh
+++ .travis.release.sh
@@ -2,12 +2,12 @@
 
 if grep -q "SNAPSHOT" version.sbt
 then
-    echo ""
+    echo "Snapshot Release just local"
 else
     rm -rf credentials.properties &&
     rm -rf project/travis-deploy-key &&
     git remote rm origin &&
-    git remote add origin git@github.com:QuadStingray/sbt-javafx.git &&
+    git remote add origin git@github.com:QuadStingray/openligadb.git &&
     git fetch origin master &&
     git checkout master &&
     git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/* &&
