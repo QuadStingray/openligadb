89d7c7d32cc956e17b4b4f9e5777b090c4bde8d1
QuadStingray <github@quadstingray.com>
2018-02-20 10:30:11 +0100

    fix in .travis.publish.sh

--- .travis.publish.sh
+++ .travis.publish.sh
@@ -2,7 +2,7 @@
 
 if grep -q "SNAPSHOT" version.sbt
 then
-    echo ""
+    echo "Snapshot Release just local"
 else
     sbt -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASSWORD publish
 fi
